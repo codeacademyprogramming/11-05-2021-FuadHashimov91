869cdac3e2af230a6cf270c854ee2da19550dae2
FuadHashimov91 <fuadbh@code.edu.az>
2021-05-11 21:45:42 +0400

    Add Html Table in JS

--- Assets/JS/register.js
+++ Assets/JS/register.js
@@ -1,6 +1,7 @@
 const email_inp = document.getElementById("email");
 const password_inp = document.getElementById("password");
 const form = document.querySelector("#form");
+const dataUser = document.querySelector("#data-user");
 let formBg = document.querySelector("#form-bg");
 
 
@@ -55,24 +56,34 @@ async function setUserData_sStorage(url) {
 setUserData_sStorage(url);
 
 function getUserData_sStorage() {
-
     let s_storage =sessionStorage.getItem("User_Data");
     let userData = JSON.parse(s_storage);
-    console.log(userData);
-
     return userData;
 }
-getUserData_sStorage();
 
 
-function createUserTable() {
+
+function createUserTable(data) {
     
+    console.log(data);
     let html = "";
 
     html = 
     `
+        <tr>
+            <td><img src="${data.picture}"/></td>
+            <td>${data.username}</td>
+            <td>${data.gender}</td>
+            <td>${data.name.title}-${data.name.first}-${data.name.last} </td>
+            <td>${data.email}</td>
+            <td>${data.age}</td>
+            <td>${data.phone}</td>
+            <td>${data.city}</td>
+            <td>${data.country}</td>
+            <td>${data.postcode}</td>
+        </tr>
     
     `
-
-
+    console.log(html)
 }
+createUserTable(getUserData_sStorage());
