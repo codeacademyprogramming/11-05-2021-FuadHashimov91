18cbe6ee6bf480af33c046b2fab62395df0ba30a
FuadHashimov91 <fuadbh@code.edu.az>
2021-05-11 22:36:50 +0400

    Refactoring FormControl Function

--- Assets/JS/login.js
+++ Assets/JS/login.js
@@ -3,24 +3,21 @@ const password_inp = document.getElementById("password");
 const form = document.querySelector("#form");
 
 
-form.addEventListener("submit",formControl);
-
 let obj ={
     name:"Admin",
     surName:"Admin"
 }
 
 
+form.addEventListener("submit",formControl);
+
+
+
 function formControl(e){
     e.preventDefault();
     let email = email_inp.value;
     let password = password_inp.value;
-    // (email === obj.name && password === obj.surName)? console.log(true):console.log(false);
-    if(email === obj.name && password === obj.surName){
-        document.location = "http://127.0.0.1:5500/Index.html";
-    }
-    else{
+    (email === obj.name && password === obj.surName)? 
+        document.location = "http://127.0.0.1:5500/Index.html":
         document.location = "http://127.0.0.1:5500/Login.html";
-        console.log(false);
-    }
 }
